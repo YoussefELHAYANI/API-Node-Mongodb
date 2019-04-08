b6ff279b4d478f6406517c6aae3b27f575f199a7
youssef <yousssef.elhayanii@gmail.com>
2019-04-08 09:45:22 +0100

    auth passport setup 2

--- models/authenticate.js
+++ models/authenticate.js
@@ -2,7 +2,14 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
-
+/*
+we are doing body-parser so that'll be added into the body of the message and then from there passport we'll retrieve that and then use that and supply 
+the username and password as parameters to the verify function that we will supply to the LocalStrategy
+*/
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+/*
+These two functions they serialize user and deserialize user are provided on the user schema and model by the use of the passport-local-mongoose
+plugin here
+*/
--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
+
+// field username/password added by passportLocalMongosse
 var User = new Schema({
     admin:   {
         type: Boolean,
@@ -25,5 +27,8 @@ var User = new Schema({
         default: false
     }
 });*/
+
+// this will automatically as I said adding support for username and hashed storage of the password using the hash and salt
+// and adding additional methods on the user schema and the model which are useful for passport authentication
 User.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User', User);
