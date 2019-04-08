0c86db64c87b4f98816cdf26e209920201f9d82a
youssef <yousssef.elhayanii@gmail.com>
2019-04-08 09:44:09 +0100

    passport auth setup

---
+++ models/authenticate.js
@@ -0,0 +1,8 @@
+// configuration file for authentication
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var passport = require('passport');
+
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      //  construct a json object with the error as the value for the error property in there and then send this back.
+      res.json({err: err});
+    }
+    else {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, status: 'Registration Successful!'});
+      });
+    }
+  });
+});
+/*
+when the router post comes in on the login endpoint, we will first call the passport authenticate local. If this is successful then this will come
+in and the next function that follows will be executed. If there is any error in the authentication, this passport authenticate local will automatically 
+send back a reply to the client about the failure of the authentication
+*/
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'You are successfully logged in!'});
+});
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
+});
 module.exports = router;
